99fa73e4263bcb4e63adc0dcbc99bc7a522edff0
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-24 13:53:05 +0200

    added some methods

--- Exam02/Exam.cs
+++ Exam02/Exam.cs
@@ -11,8 +11,15 @@ namespace Exam02
 
         public short Time { get; set; }
         public short NumOfQuestions { get; set; }
+        public Subject ExamSubject { get; set; }
 
+        public virtual void ShowExam() {
 
+            Console.WriteLine($"Exam of {ExamSubject.SubjectName}");
+            Console.WriteLine($"Time of the Exam: {Time} minutes"); // change to hours and minutes later
+            Console.WriteLine($"Number of Questions: {NumOfQuestions}");
+        
+        }
 
     }
 }
