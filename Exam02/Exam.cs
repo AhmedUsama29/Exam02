e104d1e930e2328d8a3961ec27cd5d87c849835e
Ahmed Osama <ahmed418253@feng.bu.edu.eg>
2025-01-24 13:22:48 +0200

    the base

---
+++ Exam02/Exam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam02
+{
+    internal class Exam
+    {
+
+        public short Time { get; set; }
+        public short NumOfQuestions { get; set; }
+
+
+
+    }
+}
